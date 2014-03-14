62e619d0a0b219c8b0b71852a64fcecda49944a7
Israel Antezana R <israelantezana@gmail.com>
2014-03-14 12:14:16 -0400

    Pequeños cambios para que funcione main.rb

--- lib/consola.rb
+++ lib/consola.rb
@@ -3,10 +3,6 @@ class Consola
     print(cadena)
   end
   def imprimir_cheque(cheque)
-    if(cheque.fecha_emision.next_day.day!=1)
-      imprimir("No se pudo imprimir el cheque, porque aun no es fin de mes")
-    else
-      imprimir("Nombre completo: #{cheque.beneficiario}\nCi: #{cheque.ci}\nMonto a cobrar: #{cheque.monto}$\nFecha emision:"+Date.today.to_s+"\nFirma:___________________________")
-    end
+    imprimir("Nombre completo: #{cheque.beneficiario}\nCi: #{cheque.ci}\nMonto a cobrar: #{cheque.monto}$\nFecha emision:"+Date.today.to_s+"\nFirma:___________________________")
   end
 end
